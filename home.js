7bac6d88d4414329dbd89d6b971e43001895ea0f
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-25 19:16:28 -0500

    V1

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! - Wilson</Text>
+      <Text>Wilson - from Home component</Text>
       <StatusBar style="auto" />
     </View>
   );
